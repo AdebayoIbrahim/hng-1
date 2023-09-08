953dcc676bd5ebd870921b35236ed9d0cdc166bf
SetUp <98415538+AdebayoIbrahim@users.noreply.github.com>
2023-09-08 17:03:36 +0100

    Update app.js
    Initial data commit

---
+++ app.js
@@ -0,0 +1,15 @@
+//Getting Values needed in output.
+const date = document.querySelector("#Date")
+const milliseconds  = document.querySelector('#milliseconds')
+
+//setting Date Arrays
+
+const dateArray = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+] 
